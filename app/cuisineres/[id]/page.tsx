e50890d8f2acdaa5f00bdbb31a51d926c72389bf
HT DIGITAL <htdigital@macbookair-3.home>
2026-04-02 16:30:19 +0200

    fix react import

--- app/cuisineres/[id]/page.tsx
+++ app/cuisineres/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect,useState<any[]>([])} from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'next/navigation'
 import { supabase } from '@/lib/supabase'
 
